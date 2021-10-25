2d14ece47d8eff27e0d4fc1f517dcc49b4bdccda
PrideZH <332842890@qq.com>
2021-10-25 23:41:26 +0800

    集成 Swagger 框架
    1. 修改model格式 vo为controller接收前端对象 dto为service返回controller的对象
    2. 修改Resylt响应对象为泛型 方便文档显示

---
+++ src/main/java/com/pengfu/mynav/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.pengfu.mynav.config;
+
+/**
+ * @author PrideZH
+ * @date 2021/10/25 - 22:41
+ */
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/pengfu/mynav/model/dto/UserDTO.java
@@ -0,0 +1,8 @@
+package com.pengfu.mynav.model.dto;
+
+/**
+ * @author PrideZH
+ * @date 2021/10/25 - 23:26
+ */
+public class UserDTO {
+}
